1e637134ac1985c2d6d3f9ab61d4817e5e0b6a8c
Aleksandr Vakulenko <sasha705@mail.ru>
2026-03-03 14:29:07 +0300

    rename some functions

--- Short_sig_period_calc/TEST_look_at_results.m
+++ Short_sig_period_calc/TEST_look_at_results.m
@@ -2,13 +2,15 @@
 
 
 D = Result.f_div_ppm;
-Amp = poly3calc(Result.amp_poly, T_arr);
-Phi = poly3calc(Result.phi_poly, T_arr);
-BG = poly3calc(Result.bg_poly, T_arr);
+Amp = poly3calc(Result.amp_poly, T_arr, Period);
+Phi = poly3calc(Result.phi_poly, T_arr, Period);
+BG = poly3calc(Result.bg_poly, T_arr, Period);
 
 Freq2 = Freq*(1+D/1e6);
 
 ym = Amp.*sin(2*pi*Freq2*T_arr + Phi/180*pi) + BG;
+
+Residuals = V_arr-ym;
 
 
 figure('position', [98 155 742 874])
@@ -16,9 +18,11 @@ subplot(2, 1, 1)
 hold on
 plot(T_arr, V_arr, '-b')
 plot(T_arr, ym, '--r', 'LineWidth', 2)
+title('Signal')
 
 subplot(2, 1, 2)
-plot(T_arr, V_arr-ym, '-b')
+plot(T_arr, Residuals, '-b')
+title('Residuals')
 
 
 figure('position', [871 333 746 696])
@@ -41,16 +45,20 @@ plot(T_arr, Props.bg, '-b')
 plot(T_arr, BG, '--r', 'LineWidth', 2)
 title('background')
 
+subplot(2, 2, 4)
+histogram(Residuals, 'Normalization', 'pdf')
+title('Residuals histogram')
 
 
 
-function y = poly3calc(poly, x)
-p1 = poly.p1;
-p2 = poly.p2;
-p3 = poly.p3;
 
-y = p1*x.^2 + p2*x + p3;
 
+function y = poly3calc(poly, x, Period)
+    p1 = poly.p1;
+    p2 = poly.p2;
+    p3 = poly.p3;
+    
+    y = p1*(x/Period).^2 + p2*(x/Period) + p3;
 end
 
 
