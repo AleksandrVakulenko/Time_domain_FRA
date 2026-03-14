a39f7de8102593da91da56a2007d0d81dbaf7e3b
Aleksandr Vakulenko <sasha705@mail.ru>
2026-03-14 09:26:08 +0300

    update fft_plot: multiple data plot

--- fft_plot.m
+++ fft_plot.m
@@ -8,8 +8,6 @@ arguments
     phi_limit = []
 %     fig = []
 end
-% [amp, freq] = fft_calc(sig, fs);
-[amp, freq, phi, phi_limit] = fft_calc(sig, fs, phi_limit);
 
 figure('position', [522 281 631 666])
 % if isempty(fig)
@@ -18,21 +16,39 @@ figure('position', [522 281 631 666])
 %     figure(fig)
 % end
 
-subplot(2, 1, 1)
-plot(freq, amp)
-set(gca, 'xscale', 'log')
-set(gca, 'yscale', 'log')
-xlabel('f, Hz')
-ylabel('amp')
-if ~isempty(phi_limit)
-    yline(phi_limit)
+[N, ind] = min(size(sig));
+if ind == 2
+    sig = sig';
 end
 
+for i = 1:N
+    % [amp, freq] = fft_calc(sig, fs);
+    [amp, freq, phi, phi_limit] = fft_calc(sig(i, :), fs, phi_limit);
+
+    subplot(2, 1, 1)
+    hold on
+    plot(freq, amp)
+    set(gca, 'xscale', 'log')
+    set(gca, 'yscale', 'log')
+    xlabel('f, Hz')
+    ylabel('amp')
+    if ~isempty(phi_limit)
+        yline(phi_limit)
+    end
+
+    subplot(2, 1, 2)
+    hold on
+    plot(freq, phi)
+    set(gca, 'xscale', 'log')
+    % set(gca, 'yscale', 'log')
+    xlabel('f, Hz')
+    ylabel('phi, [deg]')
+end
+
+subplot(2, 1, 1)
+hold off
 subplot(2, 1, 2)
-plot(freq, phi)
-set(gca, 'xscale', 'log')
-% set(gca, 'yscale', 'log')
-xlabel('f, Hz')
-ylabel('phi, [deg]')
+hold off
+
 end
 
