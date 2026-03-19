11bcad841a33ac5624e16d892890b3689d0c6854
Aleksandr Vakulenko <sasha705@mail.ru>
2026-03-19 16:28:06 +0300

    update Aster test

--- TEST_Aster_con.m
+++ TEST_Aster_con.m
@@ -1,22 +1,41 @@
 
+%% Run once
 
 Fern.load('aDevice')
+Fern.load('Common') 
+
+%% Measure by LCR
+
+Aster = Aster_dev(3);
+
+err = [];
+try
+    Aster.set_connection_mode("LCR");
+catch err
+    delete(Aster);
+    rethrow(err)
+end
+
+if isempty(err)
+    delete(Aster);
+end
 
 
-%%
 
+%% Get data from Sater
 % FIXME: [in Aster_dev:] read_data forces dev to measure single point
 
 clc
 Aster = Aster_dev(3);
 
-
 Aster.set_connection_mode("I2V");
-Aster.set_sensitivity(1/1e3);
+Sense = Aster.set_sensitivity(1/40e3);
+disp(['Range: ' num2str(5/Sense)])
+
 Aster.initiate();
 pause(0.2);
 
-% Aster.CMD_data_stream(1);
+Aster.CMD_data_stream(1);
 
 adev_utils.Wait(1, 'Wait for data gathering ...');
 
@@ -24,16 +43,18 @@ adev_utils.Wait(1, 'Wait for data gathering ...');
 pause(0.05);
 
 
-[Time_arr, V1_arr, V2_arr] = Aster.get_CV();
-% Aster.CMD_data_stream(0);
+% [Time_arr, V1_arr, V2_arr] = Aster.get_CV();
+[Time_arr, V1_arr, V2_arr, Scale] = Aster.get_VV();
+Aster.CMD_data_stream(0);
 
 Aster.terminate();
 delete(Aster);
 
 % V1_arr = V1_arr - mean(V1_arr);
 Time_arr = Time_arr - Time_arr(1);
+V2_arr = -V2_arr;
 
-%%
+%
 
 figure('position', [303 235 768 797])
 
@@ -46,5 +67,142 @@ subplot(2, 1, 2)
 hold on
 plot(Time_arr, V2_arr, '.-b')
 
+%% Load data to FRA_dummy_dev
 
+Save_data_flag = false;
+freq = 30;
+Fs = 10e3;
+Synth_time = Time_arr;
+Synth_signal_1 = V1_arr;
+Synth_signal_2 = V2_arr;
+
+
+FRA_dev = test_gen.FRA_dummy_dev(Synth_time, Synth_signal_1, Synth_signal_2);
+
+
+%% Show fit result
+
+
+
+Output = calc_output(Result_1, []);
+Volt1 = Output.amp;
+Volt1_err = Output.amp_err;
+P1 = Output.phi;
+P1e = Output.phi_err;
+
+Output = calc_output(Result_2, []);
+Volt2 = Output.amp;
+Volt2_err = Output.amp_err;
+P2 = Output.phi;
+P2e = Output.phi_err;
+
+
+clc
+
+Cur = Volt2*Scale;
+Cur_err = Volt2_err*Scale;
+Res = Volt1/Cur;
+Res_err = sqrt(((1/Cur)*Volt1_err)^2 + ((-Volt1/Cur^2)*Cur_err)^2);
+Phase_diff = (P1) - (P2);
+Phase_diff_error = sqrt(P1e^2 + P2e^2);
+
+% Res_cplx = Res*cos(Phase_diff/180*pi) + Res*1i*sin(Phase_diff/180*pi);
+% Res
+% Res_r = real(Res_cplx)
+% Res_i = -imag(Res_cplx)
+
+
+% Cap = 1/(6.28*freq*Res_i);
+% Cap_2 = 1/(6.28*freq*(Res+Res_err))
+% Cap_3 = 1/(6.28*freq*(Res-Res_err))
+% Cap_err = 1/(6.28*freq*Res_i^2)*Res_err;
+
+
+disp(['|R| = ' num2str(Res/1e3, '%0.3f') ' ± ' ...
+    num2str(Res_err/1e3, '%0.3f') ' kOhm'])
+% disp(['C = ' num2str(Cap*1e9, '%0.2f') ' ± ' ...
+%     num2str(Cap_err*1e9, '%0.2f') ' nF'])
+disp(['P = ' num2str(Phase_diff, '%0.2f') ' ± ' ...
+    num2str(Phase_diff_error, '%0.2f') ' deg'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% -------------------------------------------------------------
+
+
+% FIXME: shared with TEST_look_at_res2.m and TEST_look_at_results.m
+function Output = calc_output(Result_in, pps)
+arguments
+    Result_in
+    pps = []
+end
+    T_start = Result_in.amp_poly.x(1);
+    T_end = Result_in.amp_poly.x(3);
+    Length = T_end - T_start;  % [s]
+
+    flag = false;
+    if isempty(pps)
+        pps = 5;
+        flag = true;
+    else
+
+    end
+
+    N = round(Length * pps);
+    if N < 1
+        N = 5;
+        flag = true;
+    end
+    T_arr = linspace(T_start, T_end, N);
+    
+    Amp = fit_viewer.poly3calc(Result_in.amp_poly, T_arr);
+    Phi = fit_viewer.poly3calc(Result_in.phi_poly, T_arr);
+    BG = fit_viewer.poly3calc(Result_in.bg_poly, T_arr);
+    Amp_err = fit_viewer.poly3calc(Result_in.amp_poly_err, T_arr);
+    Phi_err = fit_viewer.poly3calc(Result_in.phi_poly_err, T_arr);
+    BG_err = fit_viewer.poly3calc(Result_in.bg_poly_err, T_arr);
+
+    if flag
+        Amp_err = sqrt(std(Amp)^2 + mean(Amp_err).^2);
+        Phi_err = sqrt(std(Phi)^2 + mean(Phi_err).^2);
+        BG_err = sqrt(std(BG)^2 + mean(BG_err).^2);
+
+        Amp = mean(Amp);
+        Phi = mean(Phi);
+        BG = mean(BG);
+
+        T_arr = mean(T_arr);
+    end
+
+    Output.amp = Amp;
+    Output.amp_err = Amp_err;
+    Output.phi = Phi;
+    Output.phi_err = Phi_err;
+    Output.bg = BG;
+    Output.bg_err = BG_err;
+    Output.time = T_arr;
+    Output.freq = Result_in.freq;
+    Output.debug_result = Result_in;
+    Output.single_flag = flag;
+end
 
---
+++ TEST_look_at_res2.m
@@ -0,0 +1,320 @@
+
+clc
+
+% % T_arr = T_arr;
+% % Residuals_in = Residuals;
+
+Show_channel = 2;
+
+Data_time = T_arr;
+
+if  1 == Show_channel
+    Data_signal = V1_arr;
+    Result_in = Result_1;
+elseif Show_channel == 2
+    Data_signal = V2_arr;
+    Result_in = Result_2;
+else
+    error('wrong channel number')
+end
+
+
+T_arr_min = linspace(T_arr(1), T_arr(end), 1000);
+
+[ym, Amp_full, Phi_full, BG_full] = fit_viewer.calc_fitted_signal(Result_in, T_arr);
+
+Harm_disp(Result_in);
+
+Harm_y = Harm_calc(Result_in, T_arr);
+if ~isempty(Harm_y)
+    ym = ym + Harm_y;
+end
+Residuals_in = Data_signal - ym;
+
+Noise_amp = noise_amp_calc(freq, Data_time, Data_signal, Fs);
+
+[~, Amp, Phi, BG, Amp_err, Phi_err, BG_err] = ...
+    fit_viewer.calc_fitted_signal(Result_in, T_arr_min);
+
+
+% Calc output values and errors ------------------------------------
+pps = [];
+Output = calc_output(Result_in, pps);
+Amp_out = Output.amp;
+Phi_out = Output.phi;
+BG_out = Output.bg;
+Amp_err_out = Output.amp_err;
+Phi_err_out = Output.phi_err;
+BG_err_out = Output.bg_err;
+T_out = Output.time;
+
+Freq_div = Result_in.f_div_ppm;
+Freq_div_err = Result_in.f_dev_ppm_err;
+disp([num2str(Freq_div, '%0.1f') ' ± ' num2str(Freq_div_err, '%0.1f') ' ppm'])
+
+
+if Output.single_flag
+    disp([newline 'Calc values (mean):'])
+    Str = err_str(Amp_out, Amp_err_out);
+    disp(['A = ' Str ' [V]'])
+    Str = err_str(Phi_out, Phi_err_out);
+    disp(['P = ' Str ' [deg]'])
+    Str = err_str(BG_out, BG_err_out);
+    disp(['C = ' Str ' [V]'])
+
+    SNR = 20*log10(Amp_out/Noise_amp);
+    disp([newline 'Noise level:'])
+    disp(['Noise amp = ' num2str(Noise_amp*1e3, '%0.2f') ' mV'])
+    disp(['SNR = ' num2str(Amp_out/Noise_amp, '%0.2f')])
+    disp(['SNR = ' num2str(SNR, '%0.2f') ' dB'])
+
+end
+%-------------------------------------------------------------------
+
+
+% Residuals = V_arr-ym;
+
+
+figure('position', [98 155 742 874])
+subplot(2, 1, 1)
+hold on
+plot(Data_time, Data_signal, '-b')
+plot(T_arr, ym, '--r', 'LineWidth', 2)
+plot(T_arr, BG_full, '--k', 'LineWidth', 1)
+plot(T_arr, BG_full+Amp_full, '--k', 'LineWidth', 1)
+plot(T_arr, BG_full-Amp_full, '--k', 'LineWidth', 1)
+title('Signal')
+
+subplot(2, 1, 2)
+plot(T_arr, Residuals_in, '-b')
+yline(std(Residuals_in)*2)
+title('Residuals')
+
+
+figure('position', [871 333 746 696])
+
+subplot(2, 2, 1)
+hold on
+plot(T_arr_min, Amp, '--r', 'LineWidth', 2)
+plot(T_arr_min, Amp+Amp_err, '-k', 'LineWidth', 0.5)
+plot(T_arr_min, Amp-Amp_err, '-k', 'LineWidth', 0.5)
+errorbar(T_out, Amp_out, Amp_err_out, '.m', 'MarkerSize', 12);
+title('Amp')
+
+subplot(2, 2, 2)
+hold on
+plot(T_arr_min, Phi, '--r', 'LineWidth', 2)
+plot(T_arr_min, Phi+Phi_err, '-k', 'LineWidth', 0.5)
+plot(T_arr_min, Phi-Phi_err, '-k', 'LineWidth', 0.5)
+errorbar(T_out, Phi_out, Phi_err_out, '.m', 'MarkerSize', 12);
+title('Phi, deg')
+% Max = max([Props.phi Phi]);
+% Min = min([Props.phi Phi]);
+% Max = Max + abs(Max-Min)*1.1;
+% Min = Min - abs(Max-Min)*1.1;
+% ylim([Min Max])
+
+subplot(2, 2, 3)
+hold on
+plot(T_arr_min, BG, '--r', 'LineWidth', 2)
+plot(T_arr_min, BG+BG_err, '-k', 'LineWidth', 0.5)
+plot(T_arr_min, BG-BG_err, '-k', 'LineWidth', 0.5)
+errorbar(T_out, BG_out, BG_err_out, '.m', 'MarkerSize', 12);
+title('background')
+
+subplot(2, 2, 4)
+histogram(Residuals_in, 'Normalization', 'pdf')
+title('Residuals histogram')
+
+
+
+
+%%
+HHH = Harm_calc(Result, T_arr);
+plot(T_arr, HHH);
+%%
+
+
+
+
+function Output = calc_output(Result_in, pps)
+arguments
+    Result_in
+    pps = []
+end
+    T_start = Result_in.amp_poly.x(1);
+    T_end = Result_in.amp_poly.x(3);
+    Length = T_end - T_start;  % [s]
+
+    flag = false;
+    if isempty(pps)
+        pps = 5;
+        flag = true;
+    else
+
+    end
+
+    N = round(Length * pps);
+    if N < 1
+        N = 5;
+        flag = true;
+    end
+    T_arr = linspace(T_start, T_end, N);
+    
+    Amp = fit_viewer.poly3calc(Result_in.amp_poly, T_arr);
+    Phi = fit_viewer.poly3calc(Result_in.phi_poly, T_arr);
+    BG = fit_viewer.poly3calc(Result_in.bg_poly, T_arr);
+    Amp_err = fit_viewer.poly3calc(Result_in.amp_poly_err, T_arr);
+    Phi_err = fit_viewer.poly3calc(Result_in.phi_poly_err, T_arr);
+    BG_err = fit_viewer.poly3calc(Result_in.bg_poly_err, T_arr);
+
+    if flag
+        Amp_err = sqrt(std(Amp)^2 + mean(Amp_err).^2);
+        Phi_err = sqrt(std(Phi)^2 + mean(Phi_err).^2);
+        BG_err = sqrt(std(BG)^2 + mean(BG_err).^2);
+
+        Amp = mean(Amp);
+        Phi = mean(Phi);
+        BG = mean(BG);
+
+        T_arr = mean(T_arr);
+    end
+
+    Output.amp = Amp;
+    Output.amp_err = Amp_err;
+    Output.phi = Phi;
+    Output.phi_err = Phi_err;
+    Output.bg = BG;
+    Output.bg_err = BG_err;
+    Output.time = T_arr;
+    Output.freq = Result_in.freq;
+    Output.debug_result = Result_in;
+    Output.single_flag = flag;
+end
+
+
+function disp_harm_info(harm, Freq)
+hn = harm.n;
+A = harm.amp;
+P = harm.phi;
+disp(['Harmonic ' num2str(hn) ':'])
+disp(['Freq = ' num2str(hn*Freq) ' Hz'])
+disp(['A = ' num2str(A) ' V' ...
+      newline ...
+      'P = ' num2str(P) ' deg' newline])
+end
+
+function Harm_check(Result, Props)
+
+Freq = Props.freq;
+P_harm = Props.harm;
+R_harm = Result.harm;
+R_harm_err = Result.harm_err;
+
+disp([newline 'Harmonics:' newline '-----------------------'])
+if isempty(R_harm)
+    disp([newline 'No harmonic in result struct' newline])
+    return
+else
+end
+
+if isempty(P_harm)
+    disp('Wrong harmonics:')
+    for i = 1:numel(R_harm)
+        disp_harm_info(R_harm(i), Freq)
+    end
+else
+    for i = 1:numel(P_harm)
+        hn = P_harm(i).n;
+        ind = find([R_harm.n] == hn);
+        if ~isempty(ind)
+            disp([' Right harmonic: [' num2str(hn) ']'])
+
+            if ~isnan(R_harm_err(ind).amp)
+                Str = err_str(R_harm(ind).amp, R_harm_err(ind).amp);
+                disp(['A = ' Str ' [V]'])
+            else
+                disp(['A = ' num2str(R_harm(ind).amp) ' ± NaN [V]'])
+            end
+
+            if ~isnan(R_harm_err(ind).phi)
+                Str = err_str(R_harm(ind).phi, R_harm_err(ind).phi);
+                disp(['P = ' Str ' [deg]'])
+            else
+                disp(['A = ' num2str(R_harm(ind).phi) ' ± NaN [deg]'])
+            end
+
+            disp(' Real values:')
+            disp(['A = ' num2str(P_harm(i).amp) ' [V]'])
+            disp(['P = ' num2str(P_harm(i).phi) ' [deg]'])
+            disp(' ')
+
+            R_harm(ind) = [];
+            R_harm_err(ind) = [];
+        else
+            disp('NO harmonic:')
+            disp(['(for) Props harmonic:' '[' num2str(P_harm(i).n) ']'])
+            disp_harm_info(P_harm(i), Freq);
+            disp(' ')
+        end
+    end
+
+    if ~isempty(R_harm)
+    disp('Wrong harmonics:')
+    for i = 1:numel(R_harm)
+        disp_harm_info(R_harm(i), Freq)
+    end
+    end
+    
+disp(['-----------------------' newline])
+end
+
+end
+
+
+function Harm_disp(Result_in)
+Harm = Result_in.harm;
+if ~isempty(Harm)
+    Freq = Result_in.freq;
+%     Freq_dev = Result_in.f_div_ppm;
+%     Freq = Freq * (1 + Freq_dev/1e6);
+    for i = 1:numel(Harm)
+        hn = Harm(i).n;
+        A = Harm(i).amp;
+        P = Harm(i).phi;
+        disp(['Harmonic ' num2str(hn) ':'])
+        disp(['Freq = ' num2str(hn*Freq) ' Hz'])
+        disp(['A = ' num2str(A) ' V' ...
+              newline ...
+              'P = ' num2str(P) ' deg' newline])
+    end
+end
+
+end
+
+function out = Harm_calc(Result_in, Time)
+Harm = Result_in.harm;
+if ~isempty(Harm)
+    Freq = Result_in.freq;
+    Freq_dev = Result_in.f_div_ppm;
+    Freq = Freq * (1 + Freq_dev/1e6);
+    out = zeros(size(Time));
+    for i = 1:numel(Harm)
+        hn = Harm(i).n;
+        A = Harm(i).amp;
+        P = Harm(i).phi;
+        H_value = A*sin(2*pi*hn*Freq*Time + P/180*pi);
+        out = out + H_value;
+    end
+else
+    out= [];
+end
+
+end
+
+
+
+
+
+
+
